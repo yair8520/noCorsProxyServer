6f012ba22749ed2ef5cc83649f42d9132f99fa2a
yair8520 <yair8520@gmail.com>
2023-07-28 15:11:47 +0300

    webhook

--- dist/routes/homeRoutes.js
+++ dist/routes/homeRoutes.js
@@ -97,7 +97,7 @@ router.get('/', (req, res) => {
       <p>The NoCORS Proxy Server ensures that the appropriate CORS headers are added to the response, allowing your
         client-side application to access the requested API data without CORS-related issues.</p>
       
-    <p>test 2</p>
+    <p>test 3</p>
     </body>
     
     </html>
